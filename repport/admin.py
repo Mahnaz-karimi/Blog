df44d3dc055a5ecfe6e75fd3df89f72a04417258
mk2020 <mahnaaz85@gmai.com>
2021-01-14 06:48:37 +0100

    make class aand get data from database

--- repport/admin.py
+++ repport/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+# her kan vi Register vores model for at vises in vores admin page.
+
+from .models import Post
+
+admin.site.register(Post) # så bliver vi i stand til at se Post i admin page
--- repport/views.py
+++ repport/views.py
@@ -1,25 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-posts = [
-    {
-        'author': 'Sam',
-        'title': 'rapport 1',
-        'content': 'First post content',
-        'date_posted': 'Sep 7, 2020'
-    },
-    {
-        'author': 'M k',
-        'title': 'Reppot Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
+
+from .models import Post # . menes directory to the models. importe Post class
+
 def home(request):
     context = {
-        'posts': posts
+        #'posts': posts # istedet for dommy data vi hente data fra database i det næste linje
+        'posts': Post.objects.all()
     }
     return render(request, 'repport/home.html', context)
 
 
 def about(request):
     return render(request, 'repport/about.html', {'title': 'About'})
+
+'''
+posts = [
+    {
+        'author': 'Sam',
+        'title': 'rapport 1',
+        'content': 'First post content',
+        'date_posted': 'Sep 7, 2020'
+    }
+]
+'''
