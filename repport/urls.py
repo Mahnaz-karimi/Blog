c287b060c82b8d7fcaac2145c419be7bcbe2dcfe
mk2020 <mahnaaz85@gmai.com>
2021-01-19 20:08:07 +0100

    update and  delete of post is added

--- repport/urls.py
+++ repport/urls.py
@@ -1,8 +1,19 @@
 
 from django.urls import path
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='repport-home'),
+    path('', PostListView.as_view(), name='repport-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),# det er nødvendig sti pejer på en rigtig view så derfor adder vi as_views to den method vi har brugt. i sted for pk kan også stor object atribitut
+    path('post/new/', PostCreateView.as_view(), name='post-create'), # primrry key af post
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='repport-about'),
 ]
--- repport/views.py
+++ repport/views.py
@@ -1,7 +1,13 @@
+from .models import Post # . menes directory to the models.
 from django.shortcuts import render
-from django.http import HttpResponse
-
-from .models import Post # . menes directory to the models. importe Post class
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin # mix in sikre os at user autorotiet
+from django.views.generic import (
+    ListView,
+    DetailView, # når vil kigges efter detail of post
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 
 def home(request):
     context = {
@@ -10,9 +16,53 @@ def home(request):
     }
     return render(request, 'repport/home.html', context)
 
-
 def about(request):
     return render(request, 'repport/about.html', {'title': 'About'})
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'repport/home.html'  # <app>/<model>_<viewtype>.html  PostListView.as_view() med den mens <app> / <model>_<vietype> html- app er ligsom vores repport app. model er database og vieetype er list.
+    context_object_name = 'posts'
+    ordering = ['-date_posted'] # med - vil nyeste post vil stå først
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):# her vil vi inherit fra login som krav. når vi vil se postes skal user være logget ind ellers vil den redirctet to login side
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user # acctuele user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView): # add mixin helping også at posts skaberen får lov til update post
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author: #tjekke at den post user is log in
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/' # efter delete a object så vil redirect os to
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 '''
 posts = [
